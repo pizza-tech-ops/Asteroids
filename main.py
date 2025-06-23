efdae1ebc1f0caa091eaa585c615e80858c754f0
pizza-tech-ops <sneakyllama@protonmail.com>
2025-06-24 05:53:02 +0900

    Add Asteroid class and AsteroidField for spawning

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
-
     clock = pygame.time.Clock()
     dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill((0, 0, 0))
-
         updatable.update(dt)
         for obj in drawable:
             obj.draw(screen)
-
         pygame.display.flip()
-
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
