90c73f57b99d2aa3c7cdd833226969f4819b4841
pizza-tech-ops <sneakyllama@protonmail.com>
2025-06-26 05:20:24 +0900

    Add Shot class and player shooting with spacebar

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,18 +22,22 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
-    def move(self, dt):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
         if keys[pygame.K_w]:
-            self.move(dt)
+            thrust = pygame.Vector2(0, -1).rotate(self.rotation)
+            self.velocity += thrust * 200 * dt
         if keys[pygame.K_s]:
-            self.move(-dt)
+            thrust = pygame.Vector2(0, -1).rotate(self.rotation)
+            self.velocity -= thrust * 200 * dt
+        self.position += self.velocity * dt
